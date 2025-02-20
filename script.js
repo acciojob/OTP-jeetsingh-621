aa5978c448334fbca47b7a9cb9c1bcff9db013cf
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2025-02-20 10:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener("input", (e) => {
+    if (e.inputType === "deleteContentBackward") {
+      if (index > 0) {
+        codes[index - 1].focus();
+      }
+    } else {
+      if (code.value.length === 1 && index < codes.length - 1) {
+        codes[index + 1].focus();
+      }
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && !code.value && index > 0) {
+      codes[index - 1].focus();
+    }
+  });
+});
